93e52d66c2858c89f5b3f5c5363eb9e9f33d2d0e
曹佳豪 <cao1395654040@hotmail.com>
2023-06-10 10:50:48 +0800

    fix: fixed project cover render error.

--- components/ProjectItem.vue
+++ components/ProjectItem.vue
@@ -16,7 +16,7 @@ const props = defineProps<{
     class="transition-all color-base decoration-none overflow-hidden b-rd-1">
     <div class="flex flex-row border-base b-solid b-1 b-rd-1">
       <div class="w-76px border-base b-solid b-0 b-r-1 flex flex-col bg-body b-rd-tl-1 b-rd-bl-1">
-        <img :src="`../assets/icons/${props.projectInfo.cover}`" height="48" width="48" class="m-auto" />
+        <img :src="props.projectInfo.cover" height="48" width="48" class="m-auto" />
       </div>
       <div class="flex-1 p-4 flex flex-col overflow-hidden">
         <div class="text-5 font-bold">
