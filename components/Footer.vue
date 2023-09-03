943d08860b50aa48de711b1368b9abe66d39cf2a
曹佳豪 <cao1395654040@hotmail.com>
2023-09-04 07:28:15 +0800

    chore: publish v1.1.0

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,9 +1,14 @@
+<script setup lang="ts">
+import config from '../package.json'
+</script>
+
 <template>
   <div class="h-80px flex flex-col font-light color-fade text-3">
     <div class="flex-1" />
-    <div>
+    <div class="flex flex-row gap-2">
       <a href="#" class="decoration-dashed color-fade hover-color-primary">Leo Cao</a> © 2023.
-      <span class="m-x-2">Powered by Nuxt & Vercel.</span>
+      <div>Powered by Nuxt & Vercel.</div>
+      <div>Latest release: V{{ config.version }} </div>
     </div>
     <div class="flex-1" />
   </div>
